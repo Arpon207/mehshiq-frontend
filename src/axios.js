a176b29134b5cdae6c8e267309e10bec338c511a
Arpon207 <aarpon207@gmail.com>
2025-01-03 18:06:55 +0600

    api change

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://mehshiq-backend.vercel.app/api/",
+  baseURL: "https://mehshiq-backend.onrender.com/api/",
 });
+
+//https://mehshiq-backend.vercel.app/api/
